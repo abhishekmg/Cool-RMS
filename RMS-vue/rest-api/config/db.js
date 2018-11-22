7f68f0524ffbd379bb59acda9b34c47b66e40e09
abhishekmg <abhishekmg.12@gmail.com>
2018-11-22 11:35:34 +0530

    vue not commiting

--- RMS-vue/rest-api/config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://mg:mg1234@ds151523.mlab.com:51523/rms-db'}
-  } else {
-    module.exports = {mongoURI: 'mongodb://localhost/kitchen'}
-  }
